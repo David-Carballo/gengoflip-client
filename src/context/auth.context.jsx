a5b9ac316437f825f484e9881971eb8f6af8b303
David Carballo <dvd.cm6@gmail.com>
2024-10-16 16:07:58 +0200

    frontend auth

---
+++ src/context/auth.context.jsx
@@ -0,0 +1,60 @@
+import { createContext, useEffect, useState } from "react";
+import service from "../services/config";
+
+// Componente de contexto
+const AuthContext = createContext()
+
+// Componente envoltorio
+function AuthWrapper(props) {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [loggedUserId, setLoggedUserId] = useState(null)
+  const [isValidatingToken, setIsValidatingToken] = useState(true)
+
+  useEffect(() => {
+    authenticateUser()
+  }, [])
+
+  const authenticateUser = async () => {
+    console.log("Verificando token del usuario...")
+    try {
+      const response = await service.get("/auth/verify")
+
+      // si token valido
+      setIsLoggedIn(true)
+      setLoggedUserId(response.data._id)
+      setIsValidatingToken(false)
+
+    } catch (error) {
+      // token no es valido o no existe
+      setIsLoggedIn(false)
+      setLoggedUserId(null)
+      setIsValidatingToken(false)
+    }
+
+  }
+
+  //Datos que contiene el contexto(usuario loggeado?, idUser loggeado, función verificar token)
+  const passedContext = {
+    isLoggedIn,
+    loggedUserId,
+    authenticateUser
+  }
+
+  if (isValidatingToken) {
+    return <h3>... validando usuario</h3>
+    //todo Añadir loading o algun tipo de validación
+  }
+
+  return (
+    <AuthContext.Provider value={passedContext}>
+      {props.children}
+    </AuthContext.Provider>
+  )
+
+}
+
+export {
+  AuthContext,
+  AuthWrapper
+}
---
+++ src/pages/Login.jsx
@@ -0,0 +1,81 @@
+import service from "../services/config.js";
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/auth.context.jsx"
+
+function Login() {
+
+  const navigate = useNavigate()
+  const { authenticateUser } = useContext(AuthContext)
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("")
+
+  const handleEmailChange = (e) => setEmail(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    // ... contactar al backend para validar credenciales de usuario aqui
+    try {
+      const userCredentials = {
+        email,
+        password
+      }
+      
+      // const response = await axios.post("http://localhost:5005/api/auth/login", userCredentials)
+      const response = await service.post("/auth/login", userCredentials)
+
+      localStorage.setItem("authToken", response.data.authToken)
+
+      await authenticateUser()
+
+      navigate("/contact")
+
+    } catch (error) {
+      console.log(error)
+      if (error.response.status === 400) {
+        setErrorMessage(error.response.data.message)
+      } else {
+        //! aqui deberia haber redirección a /error
+      }
+    }
+  };
+
+  return(
+    <div id="login">
+      <h1>Formulario de Acceso</h1>
+
+      <form onSubmit={handleLogin}>
+        <label>Correo Electronico:</label>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={handleEmailChange}
+        />
+
+        <br />
+
+        <label>Contraseña:</label>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+
+        <br />
+
+        <button type="submit">Acceder</button>
+
+        {errorMessage && <p>{errorMessage}</p>}
+
+      </form>
+    </div>
+  );
+}
+
+export default Login;
